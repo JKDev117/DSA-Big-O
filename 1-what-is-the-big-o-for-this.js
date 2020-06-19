f4b1cb44ab35f17cb9893e84ceeaba2149b9a16b
Jae Kim <jckim81@yahoo.com>
2020-06-18 21:39:35 -0400

    completed drill exercise #1

---
+++ 1-what-is-the-big-o-for-this.js
@@ -0,0 +1,28 @@
+/*
+1) Determine the Big O for the following algorithm: You are sitting in a room with 15 people. 
+You want to find a playmate for your dog, preferably of the same breed. 
+So you want to know if anyone out of the 15 people have the same breed as your dog. 
+You stand up and yell out, who here has a golden retriever and would like to be a playdate for my golden.
+Someone yells - "I do, be happy to bring him over"
+
+Answer: O(1)
+
+
+2) Determine the Big O for the following algorithm: You are sitting in a room with 15 people. 
+You want to find a playmate for your dog who is of the same breed. So you want to know if anyone out of the 15 people have the same breed as your dog. 
+You start with the first person and ask him if he has a golden retriever. He says no, then you ask the next person, 
+and the next, and the next until you find someone who has a golden or there is no one else to ask.
+
+
+Answer: O(n)
+*/
+
+
+
+
+
+
+
+
+
+
