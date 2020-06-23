88766b27f137d06fff1e6389a879ad82bf7c6126
Jae Kim <jckim81@yahoo.com>
2020-06-23 05:48:04 -0400

    completed 12-iterative-version.js

--- 12-iterative-version.js
+++ 12-iterative-version.js
@@ -82,3 +82,73 @@ console.log('--------------------------------------')
 
 //5. String Splitter
 
+function stringSplit(string){
+    const array = []
+    let word = ''
+    for(let i = 0; i<string.length; i++){
+        if(string.charAt(i) != '/'){
+            word += string.charAt(i)
+        } else {
+            array.push(word)
+            word = ''
+        }
+    }
+    array.push(word)
+    return array
+}
+
+
+console.log(stringSplit("02/20/2020"))
+console.log(stringSplit(""))
+console.log(stringSplit("2020"))
+console.log('--------------------------------------')
+
+
+//6. Fibonacci
+
+function fibonacci(num){
+    const array = [1, 1]
+
+    if(num==0){
+        return []
+    } else if(num==1){
+        return [1]
+    } else if(num==2){
+        return [1, 1]
+    } else {
+        for(let i=2; i<num; i++){
+            array.push(array[i-2]+array[i-1])
+        }
+        return array
+    }
+}
+
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(7))
+console.log('--------------------------------------')
+
+
+//7. Factorial
+
+function factorial(num){
+    if(num >=0 && num < 3){
+        return num
+    }
+    if(num >= 3){
+        let val=num
+        for(let i=num-1; i>0; i--){
+            val *= i
+        }
+        return val
+    }
+}
+
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
+
